0e59923a446f96f9042684e5e957d54d20140587
bando9 <bandomega1@gmail.com>
2025-06-16 19:22:48 +0700

    feat: implementasi UI dengan tailwind

--- src/Pages/Notes.jsx
+++ src/Pages/Notes.jsx
@@ -11,25 +11,47 @@ export default function Notes() {
     e.target[0].value = "";
   };
 
+  function deleteHandler() {
+    console.log("dihapus");
+  }
+
   return (
-    <div className="">
-      <h1>Notes</h1>
-      <form onSubmit={addNoteHandler}>
-        <label>
+    <div className="p-10">
+      <h1 className=" text-2xl font-bold text-center mb-5">Notes</h1>
+      <form
+        onSubmit={addNoteHandler}
+        className="flex items-center justify-center mb-5"
+      >
+        <label className="mr-4 py-1">
           <input
             type="text"
             placeholder="Add a note"
-            // onChange={(e) => console.log(e.target.value)}
+            className="border-1 rounded-sm py-1 px-2"
           />
         </label>
 
-        <button>Add</button>
+        <button className="bg-emerald-400 px-4 text-white py-1 rounded-md cursor-pointer shadow">
+          Add
+        </button>
       </form>
 
       <div>
         <ul>
           {notes.map((value, index) => {
-            return <li key={index}>{value}</li>;
+            return (
+              <li
+                key={index}
+                className="bg-slate-100 px-2 py-1 flex justify-between mb-2"
+              >
+                <span>{value}</span>
+                <button
+                  onClick={deleteHandler}
+                  className="bg-red-400 text-white px-2 py-0.5 rounded-md cursor-pointer"
+                >
+                  Delete
+                </button>
+              </li>
+            );
           })}
         </ul>
       </div>
